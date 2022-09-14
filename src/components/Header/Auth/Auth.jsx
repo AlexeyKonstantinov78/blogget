503297d71be42027fc93e51e171e02f775df85fa
AlexeyKonstantinov78 <alex_kav@mail.ru>
2022-09-14 16:00:21 +0300

    перебросили запрос на получение данных в компонент Auth

--- src/components/Header/Auth/Auth.jsx
+++ src/components/Header/Auth/Auth.jsx
@@ -1,18 +1,35 @@
+import {useEffect} from 'react';
 import style from './Auth.module.css';
 import PropTypes from 'prop-types';
 import {ReactComponent as LoginIcon} from './img/login.svg';
 import {urlAuth} from '../../../API/auth';
 import {Text} from '../../../UI/Text';
+import {URL} from '../../../API/const';
 
-export const Auth = ({token}) => (
-  <div className={style.container}>
+export const Auth = ({token}) => {
+  useEffect(() => {
+    if (!token) return;
+
+    fetch(`${URL}/api/v1/me`, {
+      headers: {
+        Authorization: `bearer ${token}`,
+      },
+    })
+      .then(response => response.json())
+      .then(data => {
+        console.log(data);
+      });
+  }, [token]);
+
+  return (<div className={style.container}>
     {token ? (token) :
       (<Text As='a' href={urlAuth}>
         <LoginIcon className={style.svg} width={128} height={128} />
       </Text>)
     }
   </div>
-);
+  );
+};
 
 Auth.propTypes = {
   token: PropTypes.string,
