f9f3623fd9afd1b89594cdd8ecdd3c542f2ab29c
AlexeyKonstantinov78 <alex_kav@mail.ru>
2022-09-15 11:55:58 +0300

    Передача пропсов через контекст без driffing

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -5,22 +5,22 @@ import Logo from './Logo';
 import Search from './Search';
 import Auth from './Auth';
 import Heading from './Heading';
-import PropTypes from 'prop-types';
+import {tokenContext} from '../../context/tokenContext';
 
-export const Header = ({token, delToken}) => (
-  <header className={style.header}>
-    <Layout>
-      <div className={style.gridContainer}>
-        <Logo />
-        <Heading text='Главная' />
-        <Search />
-        <Auth token={token} delToken={delToken}/>
-      </div>
-    </Layout>
-  </header>
-);
-
-Header.propTypes = {
-  token: PropTypes.string,
-  delToken: PropTypes.func,
+export const Header = () => {
+  const {Consumer} = tokenContext;
+  return (
+    <header className={style.header}>
+      <Layout>
+        <div className={style.gridContainer}>
+          <Logo />
+          <Heading text='Главная' />
+          <Search />
+          <Consumer>
+            {ctx => <Auth token={ctx.token} delToken={ctx.delToken} />}
+          </Consumer>
+        </div>
+      </Layout>
+    </header>
+  );
 };
