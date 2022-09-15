1406459959ad73679dfae1514350c1cf038f7a3e
AlexeyKonstantinov78 <alex_kav@mail.ru>
2022-09-15 13:46:31 +0300

    вариант указания Provider

--- src/App.js
+++ src/App.js
@@ -1,17 +1,19 @@
 import Header from './components/Header';
 import Main from './components/Main';
+import {authContext} from './context/authContext';
 import {tokenContext} from './context/tokenContext';
 import {useToken} from './hooks/useToken';
 
 function App() {
   const [token, delToken] = useToken('');
-  const {Provider} = tokenContext;
 
   return (
-    <Provider value={{token, delToken}}>
-      <Header />
-      <Main />
-    </Provider>
+    <tokenContext.Provider value={{token, delToken}}>
+      <authContext.Provider value={{}}>
+        <Header />
+        <Main />
+      </authContext.Provider>
+    </tokenContext.Provider>
   );
 }
 
