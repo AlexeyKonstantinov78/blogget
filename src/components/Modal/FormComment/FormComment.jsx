a733ccec4b1d129e5ce10fd2ea5494076605203d
AlexeyKonstantinov78 <alex_kav@mail.ru>
2022-09-19 16:30:29 +0300

    убрали ref решение через Сотсояние

--- src/components/Modal/FormComment/FormComment.jsx
+++ src/components/Modal/FormComment/FormComment.jsx
@@ -1,16 +1,20 @@
-import React, {useContext, useRef} from 'react';
+import React, {useContext, useState} from 'react';
 import style from './FormComment.module.css';
 import {Text} from '../../../UI/Text';
 import {authContext} from '../../../context/authContext';
 
 export const FormComment = props => {
   const {auth} = useContext(authContext);
-  const formCommit = useRef(null);
+  const [value, setValue] = useState('');
 
   const sendFormCommit = (event) => {
     event.preventDefault();
-    formCommit.current.focus();
-    console.log(formCommit.current.value);
+    console.log(value);
+  };
+
+  const handleChange = (event) => {
+    event.preventDefault();
+    setValue(event.target.value);
   };
 
   return <form
@@ -19,8 +23,9 @@ export const FormComment = props => {
     <Text As='h3' size={14} tsize={18}>{auth.name}</Text>
     <textarea
       className={style.textarea}
-      value="Мой коментарий"
-      ref={formCommit}></textarea>
+      value={value}
+      onChange={handleChange}
+    />
     <button className={style.btn}>Отправить</button>
   </form>;
 };
