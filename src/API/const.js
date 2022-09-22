aa651ccd6a13d8b4737adb39acfe496c5b471a46
AlexeyKonstantinov78 <alex_kav@mail.ru>
2022-09-22 16:22:46 +0300

    fix

--- src/API/const.js
+++ src/API/const.js
@@ -5,7 +5,7 @@ state=RANDOM_STRING&redirect_uri=URI&scope=SCOPE_STRING
 
 export const URL = 'https://oauth.reddit.com';
 export const URL_AUTH = 'https://www.reddit.com/api/v1/authorize?';
-export const CLIENT_ID = 'WBvrEiR0y-Xl2-eYafTNrQ';
+export const CLIENT_ID = '';
 export const RESPONSE_TYPE = 'token';
 export const RANDOM_STRING = 'random_string';
 export const REDIRECT_URI = 'http://localhost:3000/auth';
