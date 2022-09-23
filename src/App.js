77d5519756a9f1fd0907511a80dd6af9b5caf71c
AlexeyKonstantinov78 <alex_kav@mail.ru>
2022-09-23 10:57:33 +0300

    проверка работы think

--- src/App.js
+++ src/App.js
@@ -4,10 +4,22 @@ import { useDispatch } from 'react-redux';
 import { AuthContextProvider } from './context/authContext';
 import { updateToken } from './store/tokenReducer';
 import { getToken } from './API/token';
+import { store } from './store';
+
+const time = () => dispatch => {
+  // eslint-disable-next-line key-spacing
+  dispatch({ type:'start' });
+
+  setTimeout(() => {
+    // eslint-disable-next-line key-spacing
+    dispatch({ type:'END' });
+  }, 3000);
+};
 
 function App() {
   const dispatch = useDispatch();
   dispatch(updateToken(getToken()));
+  store.dispatch(time());
   return (
     <AuthContextProvider>
       <Header />
