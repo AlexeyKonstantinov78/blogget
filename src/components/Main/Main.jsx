601bd59f0fedddb5159ada995e3931202f6cd266
AlexeyKonstantinov78 <alex_kav@mail.ru>
2022-09-24 09:31:24 +0300

    lesson10 task1 update

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -2,15 +2,12 @@ import style from './Main.module.css';
 import Layout from '../Layout';
 import Tabs from './Tabs';
 import List from './List';
-import { PostContextProvider } from '../../context/postContext';
 
 export const Main = (props) => (
   <main className={style.main}>
     <Layout>
-      <PostContextProvider>
-        <Tabs />
-        <List />
-      </PostContextProvider>
+      <Tabs />
+      <List />
     </Layout>
   </main>
 );
