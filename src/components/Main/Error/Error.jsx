7276b97d2c2552600a072548e8931bc909b2f930
AlexeyKonstantinov78 <alex_kav@mail.ru>
2022-09-28 12:08:03 +0300

    404

---
+++ src/components/Main/Error/Error.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import style from './Error.module.css';
+
+// eslint-disable-next-line arrow-body-style
+export const Error = props => {
+  return <div className={style.container}>
+    <h2 className={style.title}>404</h2>
+  </div>;
+};
--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -5,17 +5,19 @@ import List from './List';
 import { Route, Routes } from 'react-router-dom';
 import { Modal } from '../Modal/Modal';
 import { EmptyMain } from './EmptyMain/EmptyMain';
+import { Error } from './Error/Error';
 
 export const Main = (props) => (
   <main className={style.main}>
     <Layout>
       <Tabs />
       <Routes>
-        <Route index element={<EmptyMain />} />
+        <Route path='/' element={<EmptyMain />} errorElement={<Error />} />
         <Route path='/:auth' element={<EmptyMain />} />
         <Route path='/category/:page' element={<List />}>
           <Route path='post/:id' element={<Modal />} />
         </Route>
+        <Route path='*' element={<Error />} />
       </Routes>
     </Layout>
   </main>
