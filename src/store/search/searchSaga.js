c1e0cf58a9774e1fadab5a0ca240ab88f5bf1b15
AlexeyKonstantinov78 <alex_kav@mail.ru>
2022-10-13 14:40:44 +0300

    второй ваориант обработки запроса

--- src/store/search/searchSaga.js
+++ src/store/search/searchSaga.js
@@ -1,24 +1,32 @@
-import { put, takeLatest, select, call } from 'redux-saga/effects';
+import { put, takeLatest, select } from 'redux-saga/effects';
 import { URL } from '../../API/const';
 import axios from 'axios';
-import { searchRequestSuccess, SEARCH_REQUEST } from './searchAction';
+import {
+  searchRequestError,
+  searchRequestSuccess,
+  SEARCH_REQUEST } from './searchAction';
 
-const fetchSearch = async (action, token) => {
-  const request = await axios(`${URL}/search/?q=${action.search}`, {
-    headers: {
-      Authorization: `bearer ${token}`,
-    },
-  });
-
-  return request.data;
-};
-
-function* workerSearch(action) {
+function* fetchSearch(search) {
   const token = yield select(state => state.token.token);
-  const data = yield call(fetchSearch, action.search, token);
-  yield put(searchRequestSuccess(data.data));
+  try {
+    const request = yield axios(`${URL}/search/?q=${search}`, {
+      headers: {
+        Authorization: `bearer ${token}`,
+      },
+    });
+
+    yield put(searchRequestSuccess(request.data.data));
+  } catch (error) {
+    yield put(searchRequestError(error));
+  }
 }
 
+// function* workerSearch(action) {
+//   const token = yield select(state => state.token.token);
+//   const data = yield call(fetchSearch, action.search, token);
+//   yield put(searchRequestSuccess(data.data));
+// }
+
 export function* watchSearch() {
-  yield takeLatest(SEARCH_REQUEST, workerSearch);
+  yield takeLatest(SEARCH_REQUEST, fetchSearch);
 }
