77c6297f93226d6a8d530e6726200bd6e6125686
AlexeyKonstantinov78 <alex_kav@mail.ru>
2022-10-26 17:01:59 +0300

    fix const REDIRECT_URI

--- src/API/const.js
+++ src/API/const.js
@@ -8,5 +8,5 @@ export const URL_AUTH = 'https://www.reddit.com/api/v1/authorize?';
 export const CLIENT_ID = '-xMmJ9VHR1j-QlOJigN2fw';
 export const RESPONSE_TYPE = 'token';
 export const RANDOM_STRING = 'random_string';
-export const REDIRECT_URI = 'http://localhost:3000/auth';
+export const REDIRECT_URI = 'https://blogget-roan.vercel.app/auth';
 export const SCOPE = 'identity submit read';
